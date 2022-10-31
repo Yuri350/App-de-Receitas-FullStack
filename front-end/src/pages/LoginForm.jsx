8ea0845b290568a6c93e59b7c11e9b719b93ab7c
Yuri Villanueva <yuri.350@hotmail.com>
2022-10-31 14:19:25 -0300

    Login FrontEnd arrumado

--- front-end/src/pages/LoginForm.jsx
+++ front-end/src/pages/LoginForm.jsx
@@ -3,12 +3,24 @@ import React, { useState, useEffect } from 'react';
 import { Navigate } from 'react-router-dom';
 import { requestLogin, setToken, requestData } from '../services/requests';
 
-const LoginForm = () => {
+function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLogged, setIsLogged] = useState(false);
   const [failedTryLogin, setFailedTryLogin] = useState(false);
   const [isDisabled, setIsDisabled] = useState(true);
+
+  const isVerify = (emailv, senha) => {
+    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
+    const emailTest = emailRegex.test(emailv);
+    const MIN_LENGTH = 6;
+
+    if (emailTest && senha.length >= MIN_LENGTH) {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -22,25 +34,13 @@ const LoginForm = () => {
 
       const { role } = await requestData('/login/validate', { email, password });
 
-      localStorage.setItem('token',  token);
-      localStorage.setItem('role',  role);
+      localStorage.setItem('token', token);
+      localStorage.setItem('role', role);
 
       setIsLogged(true);
     } catch (error) {
       setFailedTryLogin(true);
       setIsLogged(false);
-    }
-  }
-
-  const isVerify = (email, senha) => {
-    const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
-    const emailTest = emailRegex.test(email);
-    const MIN_LENGTH = 6;
-
-    if (emailTest && senha.length >= MIN_LENGTH) {
-      setIsDisabled(false);
-    } else {
-      setIsDisabled(true);
     }
   };
 
@@ -55,38 +55,44 @@ const LoginForm = () => {
       <div className="form-inner">
         <h2>Login</h2>
         <div className="form-group">
-          <label htmlFor="email">Login</label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            onChange={e =>
-              setEmail({ email: e.target.value }) } value={ details.email } />
+          <label htmlFor="email">
+            Login
+            <input
+              type="email"
+              name="email"
+              id="email"
+              onChange={ (e) => setEmail({ email: e.target.value }) }
+              value={ email }
+            />
+          </label>
         </div>
         <div className="form-group">
-          <label htmlFor="password">Senha</label>
-          <input
-            type="password"
-            name="password"
-            id="password" onChange={e =>
-              setPassword({ password: e.target.value }) } value={ details.password } />
+          <label htmlFor="password">
+            Senha
+            <input
+              type="password"
+              name="password"
+              id="password"
+              onChange={ (e) => setPassword({ password: e.target.value }) }
+              value={ password }
+            />
+          </label>
         </div>
         <input
           type="submit"
           value="LOGIN"
-          isDisabled={ isDisabled } />
+          isDisabled={ isDisabled }
+        />
       </div>
       {(failedTryLogin)
         ? (
           <p data-testid="common_login__element-invalid-email">
-            {
-              `Email ou senha inválidos`
-            }
+            Email ou senha inválidos
           </p>
-          )
+        )
         : null}
-    </form>>
-  )
+    </form>
+  );
 }
 
 export default LoginForm;
